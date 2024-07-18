ad0f58bea2eecd0503901d83dc6e1df2f165c2d3
sophia dunkley <sophia@sophia-MacBook-Air.local>
2024-07-18 21:44:17 +0300

    Update React app with new changes

--- src/components/Savebutton.jsx
+++ src/components/Savebutton.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import html2canvas from 'html2canvas';
-import { DownloadOutlined ,HeartFilled } from '@ant-design/icons';
+import 'canvas-toBlob';
+import { DownloadOutlined, HeartFilled } from '@ant-design/icons';
 
 export default function SaveButton() {
   const canvasRef = useRef(null);
@@ -10,16 +11,15 @@ export default function SaveButton() {
 
     html2canvas(dollFrame)
       .then((canvas) => {
-        // Convert canvas to data URL representing JPG image
-        const dataURL = canvas.toDataURL('image/jpeg');
-
-        // Create a link element for downloading
-        const downloadLink = document.createElement('a');
-        downloadLink.href = dataURL;
-        downloadLink.download = 'doll_creation.jpg';
-
-        // Trigger a click on the link to download the JPG image
-        downloadLink.click();
+        canvas.toBlob((blob) => {
+          const url = URL.createObjectURL(blob);
+          const downloadLink = document.createElement('a');
+          downloadLink.href = url;
+          downloadLink.download = 'doll_creation.jpg';
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        }, 'image/jpeg');
       })
       .catch((error) => {
         console.error('Error while saving doll:', error);
@@ -29,15 +29,15 @@ export default function SaveButton() {
   return (
     <div>
       <br />
-   
-      <button className='save-button' onClick={handleSaveClick}>   <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
-      <div style={{ display: 'inline-block', position: 'relative' }}>
-      <HeartFilled style={{ fontSize: '5vh', color: '#e5e5e5' }} />
-      <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }}>< DownloadOutlined className='save' /></span>
-    </div></button>
-      
-  
+      <button className='save-button' onClick={handleSaveClick}>
+        <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+        <div style={{ display: 'inline-block', position: 'relative' }}>
+          <HeartFilled style={{ fontSize: '5vh', color: '#e5e5e5' }} />
+          <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }}>
+            <DownloadOutlined className='save' />
+          </span>
+        </div>
+      </button>
     </div>
-   
   );
 }
