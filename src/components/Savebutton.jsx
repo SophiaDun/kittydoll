42afd83fe687830256f697562bf06839203c1848
sophia dunkley <sophia@sophia-MacBook-Air.local>
2024-07-18 22:07:21 +0300

    Update React app with new changes

--- src/components/Savebutton.jsx
+++ src/components/Savebutton.jsx
@@ -1,24 +1,23 @@
 import { useRef } from 'react';
 import html2canvas from 'html2canvas';
-import 'canvas-toBlob';
 import { DownloadOutlined, HeartFilled } from '@ant-design/icons';
 
 export default function SaveButton() {
-  const canvasRef = useRef(null);
-
   const handleSaveClick = () => {
     const dollFrame = document.querySelector('.doll-frame');
 
-    html2canvas(dollFrame)
+    html2canvas(dollFrame, { useCORS: true }) 
       .then((canvas) => {
         canvas.toBlob((blob) => {
           const url = URL.createObjectURL(blob);
           const downloadLink = document.createElement('a');
           downloadLink.href = url;
           downloadLink.download = 'doll_creation.jpg';
+          
           document.body.appendChild(downloadLink);
           downloadLink.click();
           document.body.removeChild(downloadLink);
+          URL.revokeObjectURL(url); 
         }, 'image/jpeg');
       })
       .catch((error) => {
@@ -30,7 +29,6 @@ export default function SaveButton() {
     <div>
       <br />
       <button className='save-button' onClick={handleSaveClick}>
-        <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
         <div style={{ display: 'inline-block', position: 'relative' }}>
           <HeartFilled style={{ fontSize: '5vh', color: '#e5e5e5' }} />
           <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white' }}>
