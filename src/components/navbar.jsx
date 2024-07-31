1bf3ae48d3dc9e35d778d94d888edfcf1f4d6834
sophia dunkley <sophia@sophia-MacBook-Air.local>
2024-07-31 18:09:32 +0300

    css changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,45 +22,45 @@ export default function Navbar() {
        <nav>
       <ul className='nav-container'>
       <li >
-        <button className='nav-button'><Link to="/selection"><img src={SkinIcon} alt="nav-logo" /></Link></button>
+        <button className='nav-button'><Link to="/selection"><img src={SkinIcon} alt="nav-button" /></Link></button>
           
         </li>
         <li>
-            <button className='nav-button'> <Link to="/eyes"><img src={EyeIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/eyes"><img src={EyeIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/eyeshadows"><img src={EyeshadowIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/eyeshadows"><img src={EyeshadowIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/lips"><img src={LipsIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/lips"><img src={LipsIcon} alt="nav-button" /></Link></button>
          
         </li>
        
         <li>
-            <button className='nav-button'> <Link to="/hair"><img src={HairIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/hair"><img src={HairIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/eyebrows"><img src={EyebrowIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/eyebrows"><img src={EyebrowIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/blush"><img src={BlushIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/blush"><img src={BlushIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/clothes"><img src={ClothesIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/clothes"><img src={ClothesIcon} alt="nav-button" /></Link></button>
          
         </li>
         <li>
-            <button className='nav-button'> <Link to="/accessories"><img src={AccessoriesIcon} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/accessories"><img src={AccessoriesIcon} alt="nav-button" /></Link></button>
          
         </li>
 
         <li>
-            <button className='nav-button'> <Link to="/background"><img src={Background} alt="nav-logo" /></Link></button>
+            <button className='nav-button'> <Link to="/background"><img src={Background} alt="nav-button" /></Link></button>
          
         </li>
 
